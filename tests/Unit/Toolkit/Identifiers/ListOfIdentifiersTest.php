cbdbddb102c04d387a41e159979a8dfb4935ce2a
Christopher Gammie <christopher.gammie@gear4music.com>
2023-11-17 11:36:36 +0000

    docs: update copyright notices

--- tests/Unit/Toolkit/Identifiers/ListOfIdentifiersTest.php
+++ tests/Unit/Toolkit/Identifiers/ListOfIdentifiersTest.php
@@ -1,10 +1,18 @@
 <?php
 /*
- * Copyright (C) Cloud Creativity Ltd - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited.
- * Proprietary and confidential.
+ * Copyright 2023 Cloud Creativity Limited
  *
- * Written by Cloud Creativity Ltd <info@cloudcreativity.co.uk>, 2023
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 declare(strict_types=1);
@@ -95,7 +103,7 @@ class ListOfIdentifiersTest extends TestCase
 
         iterator_to_array($ids->integerIds());
     }
-    
+
     /**
      * @return void
      */
@@ -115,7 +123,7 @@ class ListOfIdentifiersTest extends TestCase
         $this->assertSame($expected, iterator_to_array($ids->stringIds()));
         $this->assertSame(['1', '2', '3'], $ids->stringIds()->toBase());
     }
-    
+
     /**
      * @return void
      */
