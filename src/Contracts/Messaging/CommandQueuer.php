9b2a90b270bc536a65fc05fa20422fbcee32676f
Christopher Gammie <chris@cloudcreativity.co.uk>
2026-06-13 14:37:26 +0100

    fix: move command queuer into messaging namespace
    This should always have been in the messaging interface, as it
    simply represent a way to dispatch a message in a non-blocking
    way. For backwards compatibility, the previous queuer contract
    has been left but marked as deprecated.

---
+++ src/Contracts/Messaging/CommandQueuer.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Copyright 2026 Cloud Creativity Limited
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+declare(strict_types=1);
+
+namespace CloudCreativity\Modules\Contracts\Messaging;
+
+interface CommandQueuer
+{
+    /**
+     * Queue a command for asynchronous dispatching.
+     */
+    public function queue(Command $command): void;
+}
